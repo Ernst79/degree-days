a143f71764cbb9f9b8d0e9d3b3a4ba201c83c9a9
Ernst79 <e.klamer@gmail.com>
2021-09-28 22:53:16 +0200

    Add sensor.py

---
+++ custom_components/degree_days/sensor.py
@@ -0,0 +1,38 @@
+"""Platform for degree days sensors."""
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers import update_coordinator
+from homeassistant.helpers.entity import StateType
+
+from . import DegreeDaysData
+from .const import DOMAIN, SENSOR_TYPES, DegreeDaysSensorEntityDescription
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Add degree days entry."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    async_add_entities(
+        DegreeDaysSensor(coordinator, description) for description in SENSOR_TYPES
+    )
+
+
+class DegreeDaysSensor(update_coordinator.CoordinatorEntity, SensorEntity):
+    """Representation of a Sensor."""
+
+    entity_description: DegreeDaysSensorEntityDescription
+
+    def __init__(
+        self,
+        coordinator: DegreeDaysData,
+        description: DegreeDaysSensorEntityDescription,
+    ) -> None:
+        """Initialize the sensor."""
+        super().__init__(coordinator)
+        self.entity_description = description
+        self._attr_name = f"{description.name}"
+        self._attr_unique_id = f"{coordinator.unique_id}_{description.key}"
+
+    @property
+    def native_value(self) -> StateType:
+        """Return the native sensor value."""
+        state = getattr(self.coordinator.data, self.entity_description.key)
+        return state
